3ca370a44e96fb05036ebfc3e18800a4cc400bc1
riya40 <priyanunna20@gmail.com>
2021-12-24 18:58:38 +0530

    Added:Contacts for Address book

---
+++ src/com/bl/adp/Contacts.java
@@ -0,0 +1,12 @@
+package com.bl.adp;
+
+public class Contacts {
+    String firstname;
+    String lastname;
+    String address;
+    String city;
+    String state;
+    String email;
+    long phonenumber;
+    int pincode;
+}
