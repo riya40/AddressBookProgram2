bcf0f4cae35c4719dc4543a859e2af81676923f9
riya40 <priyanunna20@gmail.com>
2021-12-24 18:44:49 +0530

    Added:Welcome Message

---
+++ src/com/bl/adp/AddressBook.java
@@ -0,0 +1,8 @@
+package com.bl.adp;
+
+public class AddressBook {
+    public static void main(String args[]){
+        /* address book welcome message */
+        System.out.println("Welcome to address book program");
+    }
+}
